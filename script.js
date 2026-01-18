d712dfcc65a07ee3fa650ed547eef0e98583f2bf
ankits030 <ankitsingh120904@gmail.com>
2026-01-18 15:09:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let x = "";
+let y = "";
+let operator = "";
+let numberRegister = "";
+const result = document.getElementById('result');
 
+function add (x, y) {
+    return x + y;
+}
+
+function substract (x, y) {
+    return x - y;
+}
+
+function mulitply (x, y) {
+    return x * y;
+}
+
+function divide (x, y) {
+    if (y !== 0) {
+        return x / y;
+    } else {
+        return Error;
+    }    
+}
